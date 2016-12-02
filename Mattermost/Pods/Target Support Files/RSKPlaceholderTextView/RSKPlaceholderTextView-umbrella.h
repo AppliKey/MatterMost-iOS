8af32719b93e6fae2104bd32c920365d19c4019b
Dmitry Smetankin <smetankin93@gmail.com>
2016-12-02 13:51:54 +0200

    placeholder added

---
+++ Mattermost/Pods/Target Support Files/RSKPlaceholderTextView/RSKPlaceholderTextView-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double RSKPlaceholderTextViewVersionNumber;
+FOUNDATION_EXPORT const unsigned char RSKPlaceholderTextViewVersionString[];
+
